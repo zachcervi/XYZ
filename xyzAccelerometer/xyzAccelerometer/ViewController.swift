17720df7903a139f1cb6519df2ffcda0bca481a9
Zach Cervi <zachcervi@gmail.com>
2018-10-06 12:01:03 -0600

    initial commit

---
+++ xyzAccelerometer/xyzAccelerometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyzAccelerometer
+//
+//  Created by Zach Cervi on 10/6/18.
+//  Copyright © 2018 Zach Cervi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
