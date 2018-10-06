5f6878238b8657552a01b8fd1bbb110bdcc91461
Zach Cervi <zachcervi@gmail.com>
2018-10-06 12:39:11 -0600

    added CoreMotion and Accelerometer

--- xyzAccelerometer/xyzAccelerometer/ViewController.swift
+++ xyzAccelerometer/xyzAccelerometer/ViewController.swift
@@ -7,18 +7,25 @@
 //
 
 import UIKit
-
+import CoreMotion
 class ViewController: UIViewController {
     
+    //IBOutlets
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
     
+    var motionManager: CMMotionManager! //create a motion manager
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        motionManager = CMMotionManager() //initalize CMMotionManager
+       motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
     }
-
-
+    //Update Labels
+    func updateLabels(data: CMAccelerometerData?, error: Error?){
+        guard let accelerometerData = data else {return}
+        print(accelerometerData)
+    }
 }
 
